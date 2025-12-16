e771f897c3444203fa9393e95de6da70aababc53
wjh <ojh8958@naver.com>
2025-12-16 15:25:00 +0900

    op_kwargs 연습2

--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -10,9 +10,11 @@ def regist2(name, sex, *a, **k):
     print(f'이름 : {name}')
     print(f'성별 : {sex}')
     print(f'기타 옵션들 : {a}')
-    email = k['email'] or None
-    phone = k['phone'] or None
+    # email = k['email'] or None   # 해당 키 값이 없는 경우 에러가 남
+    # phone = k['phone'] or None
+    email = k.get('email') or None # get 함수를 사용하면 키 값이 없는 경우 에러 안 나고 그냥 None 값을 반환함 더 안전한 방법
+    phone = k.get('phone') or None
     if email:
         print(email)
     if phone:
-        print(phone)
+        print(phone) 
