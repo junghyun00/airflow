ef083dfbddb2cc2227be06c3f6d6a6a349bd5861
wjh <ojh8958@naver.com>
2025-12-16 21:14:28 +0900

    b

---
+++ dags/dags_python_with_xcom_eg1.py
@@ -0,0 +1,36 @@
+from airflow.sdk import DAG, task
+import datetime
+import pendulum
+
+
+with DAG(
+    dag_id="dags_python_with_xcom_eg1",
+    schedule="10 0 * * *",  # 분 시 일 월 요일
+    start_date=pendulum.datetime(2025, 12, 1, tz="Asia/Seoul"), 
+    catchup=False
+) as dag:
+    
+    @task(task_id = 'python_xcom_push_task1')
+    def xcom_push1(**k):
+        ti = k.get('ti')
+        ti.xcom_push(key = 'result1', value = 'value1')
+        ti.xcom_push(key = 'result2', value = [1,2,3])
+
+
+    @task(task_id = 'python_xcom_push_task2')
+    def xcom_push2(**k):
+        ti = k.get('ti')
+        ti.xcom_push(key = 'result1', value = 'value2')
+        ti.xcom_push(key = 'result2', value = [1,2,3,4])
+
+
+    @task(task_id = 'python_xcom_pull_task')
+    def xcom_pull(*k):
+        ti = k.get('ti')
+        value1 = ti.xcom_pull(key='result1')
+        value2 = ti.xcom_pull(key='result2', task_ids = 'python_xcom_push_task1')
+        print(value1)
+        print(value2)
+
+
+xcom_push1() >> xcom_push2() >> xcom_pull()
---
+++ dags/dags_python_with_xcom_eg2.py
@@ -0,0 +1,32 @@
+from airflow.sdk import DAG, task
+import datetime
+import pendulum
+
+
+with DAG(
+    dag_id="dags_python_with_xcom_eg2",
+    schedule="10 0 * * *",  # 분 시 일 월 요일
+    start_date=pendulum.datetime(2025, 12, 1, tz="Asia/Seoul"), 
+    catchup=False
+) as dag:
+    
+    @task(task_id = 'python_xcom_push_by_return')
+    def xcom_push_result(**k):
+        return 'Success'
+
+
+    @task(task_id = 'python_xcom_pull_1')
+    def xcom_pull_1(**k):
+        ti = k.get('ti')
+        value1 = ti.xcom_pull(task_ids = 'python_xcom_push_by_return')
+        print('xcom_pull 메서드로 직접 찾은 리턴 값 : ' + value1)
+
+
+    @task(task_id = 'python_xcom_pull_2')
+    def xcom_pull_2(status, **k):
+        print('함수 입력값으로 받은 값 : ' + status)
+
+
+python_xcom_push_by_return = xcom_push_result()
+xcom_pull_2(python_xcom_push_by_return)
+python_xcom_push_by_return >> xcom_pull_1()
