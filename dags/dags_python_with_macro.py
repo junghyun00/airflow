6b33ec21ccbdb5308c3b0168d2884e4f7e739cbf
wjh <ojh8958@naver.com>
2025-12-16 18:47:02 +0900

    b

--- dags/dags_python_with_macro.py
+++ dags/dags_python_with_macro.py
@@ -11,8 +11,8 @@ with DAG(
 ) as dag:
     
     @task(task_id = 'task_using_macros',
-          templates_dict = { 'start_date': '{{data_interval_end.in_timezon("Asia/Seoul").subtract(month=1).replace(day=1) | ds}}'  # 전월 1일
-                           , 'end_date'  : '{{data_interval_end.in_timezon("Asia/Seoul").subtract(month=1).end_of("month") | ds}}' # 전월 말일
+          templates_dict = { 'start_date': '{{data_interval_end.in_timezone("Asia/Seoul").subtract(month=1).replace(day=1) | ds}}'  # 전월 1일
+                           , 'end_date'  : '{{data_interval_end.in_timezone("Asia/Seoul").subtract(month=1).end_of("month") | ds}}' # 전월 말일
           }
     )
     def get_datetime_macro(**kwargs):
@@ -33,8 +33,8 @@ with DAG(
 
         data_interval_end = kwargs.get('data_interval_end')
 
-        prev_month_day_first = data_interval_end.in_timezon("Asia/Seoul") + relativedelta(month = -1, day = 1)
-        prev_month_day_last  = data_interval_end.in_timezon("Asia/Seoul").replace(day = 1) + relativedelta(days = -1)
+        prev_month_day_first = data_interval_end.in_timezone("Asia/Seoul") + relativedelta(month = -1, day = 1)
+        prev_month_day_last  = data_interval_end.in_timezone("Asia/Seoul").replace(day = 1) + relativedelta(days = -1)
         print(f"prev_month_day_first : {prev_month_day_first.strftime('%Y-%m-%d')}")
         print(f"prev_month_day_last : {prev_month_day_last.strftime('%Y-%m-%d')}")
 
