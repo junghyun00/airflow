f5da484f58aba8c56ef2e15f672397945a2d3792
wjh <ojh8958@naver.com>
2025-12-16 15:11:12 +0900

    op_kwargs 연습

---
+++ dags/dags_python_with_op_kwargs.py
@@ -0,0 +1,22 @@
+from airflow.sdk import DAG, task
+import datetime
+import pendulum
+from airflow.providers.standard.operators.python import PythonOperator  # 함수를 실행시키는 오퍼레이터
+from common.common_func import regist2
+
+ 
+with DAG(
+    dag_id="dags_python_with_op_kwargs", 
+    schedule="30 6 * * *",  # 분 시 일 월 요일
+    start_date=pendulum.datetime(2025, 1, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+
+    regist_t1 = PythonOperator (
+        task_id = 'regist_t1',
+        python_callable = regist2,
+        op_args = ['wjh', 'w', 'kr', 'seoul'],
+        op_kwargs= {'email':'ojh8958@naver.com', 'phone':'010'}
+    )
+    
+    
--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -5,3 +5,14 @@ def regist(name, sex, *args):
     print(f'이름 : {name}')
     print(f'성별 : {sex}')
     print(f'기타 옵션들 : {args}')
+
+def regist2(name, sex, *a, **k):
+    print(f'이름 : {name}')
+    print(f'성별 : {sex}')
+    print(f'기타 옵션들 : {a}')
+    email = k['email'] or None
+    phone = k['phone'] or None
+    if email:
+        print(email)
+    if phone:
+        print(phone)
