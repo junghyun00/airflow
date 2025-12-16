319bafc74bc5ea47c61e1959a48f34ee680f62fd
wjh <ojh8958@naver.com>
2025-12-16 15:01:08 +0900

    op_args 연습

--- dags/dags_python_with_op_args.py
+++ dags/dags_python_with_op_args.py
@@ -14,7 +14,7 @@ with DAG(
 
     regist_t1 = PythonOperator (
         task_id = 'regist_t1',
-        python_callable = regist
+        python_callable = regist,
         op_args = ['wjh', 'w', 'kr', 'seoul']
     )
     
