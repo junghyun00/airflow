107af2994a17162060df633a51f12f1c08df22e0
wjh <ojh8958@naver.com>
2025-12-20 21:38:42 +0900

    aaaa

--- dags/dags_python_operator.py
+++ dags/dags_python_operator.py
@@ -6,7 +6,7 @@ import random
 
 with DAG(
     dag_id="dags_python_operator",
-    schedule="30 6 * * *",  # 분 시 일 월 요일
+    schedule="40 21 * * *",  # 분 시 일 월 요일
     start_date=pendulum.datetime(2025, 1, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
