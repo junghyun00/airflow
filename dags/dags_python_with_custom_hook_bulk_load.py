85a1efad1e306919cfd40f573b05af7a8384709e
wjh <ojh8958@naver.com>
2025-12-22 21:19:26 +0900

    aa

--- dags/dags_python_with_custom_hook_bulk_load.py
+++ dags/dags_python_with_custom_hook_bulk_load.py
@@ -2,18 +2,18 @@ from airflow.sdk import DAG, task
 import datetime
 import pendulum
 from airflow.providers.standard.operators.python import PythonOperator  # 함수를 실행시키는 오퍼레이터
-from hooks.custom_postgrs_hook import CustomPostgresHook  # 직접 만든 hook 호출
+from hooks.custom_postgres_hook import CustomPostgresHook  # 직접 만든 hook 호출 
 
 
 with DAG(
-    dag_id="dasg_python_whit_custom_hook_bulk_load", 
+    dag_id="dags_python_with_custom_hook_bulk_load", 
     schedule="30 6 * * *",  # 분 시 일 월 요일
     start_date=pendulum.datetime(2025, 1, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
     def insrt_postgre(postgre_conn_id, tbl_nm, file_nm, **kwagrs):
         custom_postgre_hook = CustomPostgresHook(postgres_conn_id=postgre_conn_id)
-        custom_postgre_hook.bulk_load(table_name=tbl_nm, file_nm=file_nm, delimiter='', is_header=True, is_replace=True)
+        custom_postgre_hook.bulk_load(table_name=tbl_nm, file_nm=file_nm, delimiter='', is_header=True, is_replace=True)  # 해당 테이블이 있으면 엎어쳐서 새로 올린다
 
     insrt_postgre = PythonOperator(
         task_id = 'insrt_postgre',
