1128585666356b51d4a0bb40c51b31c301e468b1
wjh <ojh8958@naver.com>
2025-12-22 22:13:27 +0900

    ddc

--- plugins/operators/seoul_api_to_csv_operator.py
+++ plugins/operators/seoul_api_to_csv_operator.py
@@ -31,6 +31,9 @@ class SeoulApiToCsvOperator(BaseOperator):
             self.log.info(f'끝:{end_row}')
             row_df = self._call_api(self.base_url, start_row, end_row)
             total_row_df = pd.concat([total_row_df, row_df])
+            
+            print(total_row_df)
+
             if len(row_df) < 1000:
                 break
             else:
