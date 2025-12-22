8fb7cb32330b79033917db4563f76f9ad23119f2
wjh <ojh8958@naver.com>
2025-12-22 17:46:14 +0900

    aa

--- dags/dags_python_with_postgres.py
+++ dags/dags_python_with_postgres.py
@@ -15,7 +15,7 @@ with DAG(
         import psycopg2
         from contextlib import closing
 
-        with closing(psycopg2.connect(host=ip, dbname=dbname, user=user, passwd=passwd, port=int(port))) as conn:
+        with closing(psycopg2.connect(host=ip, dbname=dbname, user=user, password=passwd, port=int(port))) as conn:
             with closing(conn.cursor()) as cursor:
                 dag_id = kwargs.get('ti').dag_id
                 task_id = kwargs.get('ti').task_id
