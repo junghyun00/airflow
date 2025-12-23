c77e0924962353edff2e9365409c5760fb1662d2
wjh <ojh8958@naver.com>
2025-12-23 17:10:28 +0900

    qww

--- dags/dags_seoul_api_people.py
+++ dags/dags_seoul_api_people.py
@@ -16,10 +16,9 @@ with DAG(
     tb_seoul_people = SeoulApiToCsvOperator(
         task_id='tb_seoul_people',
         dataset_nm='SPOP_LOCAL_RESD_DONG',   # 이 데이터셋 이름은 꼭 api에 있는걸로 해야함 오퍼레이터를 그렇게 만들었음
-        # path='/opt/airflow/files/SPOP_LOCAL_RESD_DONG/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
-        path='/opt/airflow/files/SPOP_LOCAL_RESD_DONG/20251219',
+        path='/opt/airflow/files/SPOP_LOCAL_RESD_DONG/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
         file_name='SPOP_LOCAL_RESD_DONG.csv',
-        base_dt = '20251219'
+        base_dt = '20251217'
     )
 
 
