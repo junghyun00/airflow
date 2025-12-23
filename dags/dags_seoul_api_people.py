24f85d9d8fd22f19b31422adef8e434f0fda2680
wjh <ojh8958@naver.com>
2025-12-23 16:46:49 +0900

    aa

--- dags/dags_seoul_api_people.py
+++ dags/dags_seoul_api_people.py
@@ -9,10 +9,10 @@ from airflow.sdk import DAG
 with DAG(
     dag_id='dags_seoul_api_people',
     schedule='0 7 * * *',
-    start_date=pendulum.datetime(2025,4,1, tz='Asia/Seoul'),
+    start_date=pendulum.datetime(2025,4,1, tz='Asia/Seoul'), 
     catchup=False
 ) as dag:
-    '''행정동 단위 서울 생활인구(내국인)'''
+    '''행정동 단위 서울 생활인구(내국인).'''
     tb_seoul_people = SeoulApiToCsvOperator(
         task_id='tb_seoul_people',
         dataset_nm='SPOP_LOCAL_RESD_DONG',   # 이 데이터셋 이름은 꼭 api에 있는걸로 해야함 오퍼레이터를 그렇게 만들었음
