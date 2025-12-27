56984dcd690e1421d4913db31674f69c17d870d2
wjh <ojh8958@naver.com>
2025-12-27 20:31:45 +0900

    a

--- dags/wjh_sur_tag_mig_test.py
+++ dags/wjh_sur_tag_mig_test.py
@@ -11,23 +11,48 @@ with DAG(
     catchup=False
 ) as dag:
 
-    def insert_postgres(postgres_conn_id, **kwargs):
+    # def insert_postgres(postgres_conn_id, **kwargs):
+    #     from airflow.providers.postgres.hooks.postgres import PostgresHook
+    #     from contextlib import closing
+
+    #     postgres_hook = PostgresHook(postgres_conn_id)
+    #     with closing(postgres_hook.get_conn()) as conn:
+    #         with closing(conn.cursor()) as cursor:
+    #             dag_id = kwargs.get('ti').dag_id
+    #             task_id = kwargs.get('ti').task_id
+    #             run_id = kwargs.get('ti').run_id
+    #             msg = 'insert 수행'
+    #             sql = 'insert into py_opr_drct_insert values (%s,%s,%s,%s);'
+    #             cursor.execute(sql, (dag_id, task_id, run_id, msg))
+    #             conn.commit()
+
+    # insrt_postgres_task = PythonOperator(
+    #     task_id = 'insrt_postgres_task',
+    #     python_callable=insert_postgres,
+    #     op_kwargs={'postgres_conn_id':'conn-target'},
+    #     connect_timeout=5
+    # )
+
+    def mig_data(sur_conn_id, tag_conn_id, **kwargs):
         from airflow.providers.postgres.hooks.postgres import PostgresHook
-        from contextlib import closing
 
-        postgres_hook = PostgresHook(postgres_conn_id)
-        with closing(postgres_hook.get_conn()) as conn:
-            with closing(conn.cursor()) as cursor:
-                dag_id = kwargs.get('ti').dag_id
-                task_id = kwargs.get('ti').task_id
-                run_id = kwargs.get('ti').run_id
-                msg = 'insert 수행'
-                sql = 'insert into py_opr_drct_insert values (%s,%s,%s,%s);'
-                cursor.execute(sql, (dag_id, task_id, run_id, msg))
-                conn.commit()
+        sur_hook = PostgresHook(sur_conn_id)
+        tag_hook = PostgresHook(tag_conn_id)
+        
+        rows = sur_hook.get_records(
+            """
+            select * from tb_bike_station_master
+            """
+        )
 
-    insrt_postgres_task = PythonOperator(
-        task_id = 'insrt_postgres_task',
-        python_callable=insert_postgres,
-        op_kwargs={'postgres_conn_id':'conn-target'}
+        tag_hook.insert_rows(
+            table_name = 'tb_bike_station_master',
+            rows = rows,
+            target_fields = ["rntls_id", "addr1", "addr2" , "lat", "lot" ]
+        )
+
+    mig_task = PythonOperator(
+        task_id = 'mig_task',
+        python_callable=mig_data,
+        op_kwargs={'sur_conn_id' : 'conn-db-postgrs-custom', 'tag_conn_id' : 'conn-target'}
     )
